7e255418730f575ba8d691e58f4cb3dd3666093a
anasmuhajir <42487942+anasmuhajir@users.noreply.github.com>
2018-08-18 12:13:51 +0500

    multiple binding
    hahahahahahahha

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Multiplebinding.Startup))]
+namespace Multiplebinding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
